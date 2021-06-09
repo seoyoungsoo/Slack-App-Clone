519550760a1fe79b06b4b92b60e41d51e3c71863
seoyoungsoo <iulove37@naver.com>
2021-06-10 00:03:20 +0900

    Feat: 코드 스플리팅과 이모션 설정

--- client/layouts/App.tsx
+++ client/layouts/App.tsx
@@ -1,7 +1,9 @@
-import React, { FC } from 'react';
+import React from 'react';
+import loadable from '@loadable/component';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import LogIn from '@pages/LogIn';
-import SignUp from '@pages/SignUp';
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
   return (
