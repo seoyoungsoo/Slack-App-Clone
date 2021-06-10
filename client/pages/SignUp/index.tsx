aed1144d220213f2f33861153843ff10502b6055
seoyoungsoo <iulove37@naver.com>
2021-06-10 14:47:33 +0900

    Feat: 회원가입 페이지 구현

--- client/pages/SignUp/index.tsx
+++ client/pages/SignUp/index.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
-import { Form, Label, Input, LinkContainer, Button, Header } from './styles';
+import useInput from '@hooks/useinput';
+import React, { useCallback, useState, VFC } from 'react';
+import { Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
 
 const SignUp = () => {
+  const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
+  const [password, , setPassword] = useInput('');
+  const [passwordCheck, , setPasswordCheck] = useInput('');
+  const [mismatchError, setMismatchError] = useState(false);
+
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck],
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value !== password);
+    },
+    [password],
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      if (!mismatchError) {
+        console.log('서버로 회원가입하기');
+      }
+    },
+    [email, nickname, password, passwordCheck],
+  );
+
   return (
     <div id="container">
       <Header>Sleact</Header>
@@ -37,14 +71,14 @@ const SignUp = () => {
           </div>
           {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {signUpError && <Error>{signUpError}</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
+          {/* {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <Link to="/login">로그인 하러가기</Link>
+        {/* <Link to="/login">로그인 하러가기</Link> */}
       </LinkContainer>
     </div>
   );
