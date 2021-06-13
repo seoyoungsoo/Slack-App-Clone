9497080c7af09160f603c56665a305eef90ecdfb
seoyoungsoo <iulove37@naver.com>
2021-06-13 22:42:22 +0900

    Feat: Workspace 구현
    - 로그인 후 워크스페이스 기본 구현

---
+++ client/layouts/Workspace.tsx
@@ -0,0 +1,34 @@
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import React from 'react';
+import { FC } from 'react';
+import { useCallback } from 'react';
+import { Redirect } from 'react-router';
+import useSWR from 'swr';
+
+const Workspace: FC = ({ children }) => {
+  const { data, error, revalidate } = useSWR('http://localhost:3095/api/users', fetcher);
+
+  const onLogout = useCallback(() => {
+    axios
+      .post('/api/users/logout', null, {
+        withCredentials: true,
+      })
+      .then(() => {
+        revalidate();
+      });
+  }, []);
+
+  if (!data) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      <button onClick={onLogout}>로그아웃</button>
+      {children}
+    </div>
+  );
+};
+
+export default Workspace;
