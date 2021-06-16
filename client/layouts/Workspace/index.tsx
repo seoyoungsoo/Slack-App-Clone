86f999446d5c98ab6590b8b730a3bf980ad0ba9b
seoyoungsoo <iulove37@naver.com>
2021-06-16 16:57:25 +0900

    Feat: 폴더 구조 변경 및 라우터 설정 변경

---
+++ client/layouts/Workspace/index.tsx
@@ -0,0 +1,69 @@
+import {
+  Channels,
+  Chats,
+  Header,
+  MenuScroll,
+  ProfileImg,
+  RightMenu,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from '@layouts/Workspace/styles';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import React from 'react';
+import { FC } from 'react';
+import { useCallback } from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+import useSWR, { mutate } from 'swr';
+import gravatar from 'gravatar';
+import loadable from '@loadable/component';
+
+const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
+
+const Workspace: FC = ({ children }) => {
+  const { data, error, revalidate } = useSWR('http://localhost:3095/api/users', fetcher);
+
+  const onLogout = useCallback(() => {
+    axios
+      .post('/api/users/logout', null, {
+        withCredentials: true,
+      })
+      .then(() => {
+        mutate('http://localhost:3095/api/users', false);
+      });
+  }, []);
+
+  if (!data) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
+      <button onClick={onLogout}>로그아웃</button>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>MenuScroll</MenuScroll>
+        </Channels>
+        <Chats>
+          <Switch>
+            <Route path="/workspace/channel" component={Channel} />
+            <Route path="/workspace/dm" component={DirectMessage} />
+          </Switch>
+        </Chats>
+      </WorkspaceWrapper>
+    </div>
+  );
+};
+
+export default Workspace;
