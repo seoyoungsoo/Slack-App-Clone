ac5d7420c23cc6145edac73d61f1eb8ea19fdc36
seoyoungsoo <iulove37@naver.com>
2021-06-22 00:20:59 +0900

    Feat: 채널 모달 컴포넌트 구현

---
+++ client/components/CreateChannelModal/index.tsx
@@ -0,0 +1,29 @@
+import Modal from '@components/Modal';
+import useInput from '@hooks/useInput';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import React, { useCallback, VFC } from 'react';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  setShowCreateChannelModal: (flag: boolean) => void;
+}
+const CreateChannelModal: VFC<Props> = ({ show, onCloseModal, setShowCreateChannelModal }) => {
+  const [newChannel, onChangeNewChannel, setNewChannel] = useInput('');
+
+  const onCreateChannel = useCallback((e) => {}, []);
+
+  return (
+    <Modal show={show} onCloseModal={onCloseModal}>
+      <form onSubmit={onCreateChannel}>
+        <Label id="channel-label">
+          <span>채널</span>
+          <Input id="channel" value={newChannel} onChange={onChangeNewChannel} />
+        </Label>
+        <Button type="submit">생성하기</Button>
+      </form>
+    </Modal>
+  );
+};
+
+export default CreateChannelModal;
