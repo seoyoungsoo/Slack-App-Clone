b1944fe6a0fe44fe9c1682ab6ddc353091a86279
seoyoungsoo <iulove37@naver.com>
2021-06-23 22:45:35 +0900

    Feat: 채널 생성 모달 구현

--- client/components/CreateChannelModal/index.tsx
+++ client/components/CreateChannelModal/index.tsx
@@ -1,7 +1,13 @@
 import Modal from '@components/Modal';
 import useInput from '@hooks/useInput';
 import { Button, Input, Label } from '@pages/SignUp/styles';
+import { IChannel, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
 import React, { useCallback, VFC } from 'react';
+import { useParams } from 'react-router';
+import { toast } from 'react-toastify';
+import useSWR from 'swr';
 
 interface Props {
   show: boolean;
@@ -10,8 +16,45 @@ interface Props {
 }
 const CreateChannelModal: VFC<Props> = ({ show, onCloseModal, setShowCreateChannelModal }) => {
   const [newChannel, onChangeNewChannel, setNewChannel] = useInput('');
+  const { workspace, channel } = useParams<{ workspace: string; channel: string }>();
+  const {
+    data: userData,
+    error,
+    revalidate,
+  } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher, {
+    dedupingInterval: 2000,
+  });
+  const { data: channelData, revalidate: revalidateChannel } = useSWR<IChannel[]>(
+    userData ? `/api/workspaces/${workspace}/channels` : null,
+    fetcher,
+  );
 
-  const onCreateChannel = useCallback((e) => {}, []);
+  const onCreateChannel = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      axios
+        .post(
+          `/api/workspaces/${workspace}/channels`,
+          {
+            name: newChannel,
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then(() => {
+          setShowCreateChannelModal(false);
+          revalidateChannel();
+          setNewChannel('');
+        })
+        .catch((err) => {
+          console.dir(err);
+          toast.error(err.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newChannel],
+  );
 
   return (
     <Modal show={show} onCloseModal={onCloseModal}>
