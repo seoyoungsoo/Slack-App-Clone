52993717b1f0232714b15b9393223132fbc543d5
seoyoungsoo <iulove37@naver.com>
2021-07-13 21:02:18 +0900

    Feat: 채널 api 구현

--- client/pages/Channel/index.tsx
+++ client/pages/Channel/index.tsx
@@ -1,10 +1,49 @@
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
+import Workspace from '@layouts/Workspace';
 import { Container, Header } from '@pages/Channel/styles';
-import React from 'react';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import React, { useCallback } from 'react';
+import { useParams } from 'react-router';
+import useSWR, { useSWRInfinite } from 'swr';
+import { IChannel, IChat, IUser, IDM } from '@typings/db';
 
 const Channel = () => {
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/user/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+  const [chat, onChangeChat, setChat] = useInput('');
+  const {
+    data: chatData,
+    mutate: mutateChat,
+    revalidate,
+  } = useSWR<IDM[]>(`/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`, fetcher);
+
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            revalidate();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat],
+  );
+
   return (
     <Container>
       <Header>채널!</Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 };
