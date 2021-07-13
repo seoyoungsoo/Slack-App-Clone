da7167f16b325c441693dbfb614448fa20d9f4bf
seoyoungsoo <iulove37@naver.com>
2021-07-13 21:02:31 +0900

    Feat: DM api 구현

--- client/pages/DirectMessage/index.tsx
+++ client/pages/DirectMessage/index.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import { Container, Header } from '@pages/DirectMessage/styles';
+import React, { useCallback } from 'react';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import { useParams } from 'react-router';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { IChannel, IChat, IUser, IDM } from '@typings/db';
 
 const DirectMessage = () => {
-  return <div>DM!</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/user/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+  const [chat, onChangeChat, setChat] = useInput('');
+  const {
+    data: chatData,
+    mutate: mutateChat,
+    revalidate,
+  } = useSWR<IDM[]>(`/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`, fetcher);
+
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            revalidate();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat],
+  );
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      {/* <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd} /> */}
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
+      {/* {dragOver && <DragOver>업로드!</DragOver>} */}
+    </Container>
+  );
 };
 
 export default DirectMessage;
