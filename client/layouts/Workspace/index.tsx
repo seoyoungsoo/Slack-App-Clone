3ce2a1652cd058309d91a76684cbdfa8a1f5a7e5
seoyoungsoo <iulove37@naver.com>
2021-07-19 22:47:43 +0900

    Feat: 워크스페이스 소켓 통신을 활용한 로그인 표시 설정

--- client/layouts/Workspace/index.tsx
+++ client/layouts/Workspace/index.tsx
@@ -16,7 +16,7 @@ import {
 } from '@layouts/Workspace/styles';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
-import React, { VFC, useCallback, useState } from 'react';
+import React, { VFC, useCallback, useState, useEffect } from 'react';
 import { Redirect, Route, Switch, useParams } from 'react-router';
 import useSWR, { mutate } from 'swr';
 import gravatar from 'gravatar';
@@ -33,6 +33,7 @@ import InviteWorkspaceModal from '@components/InviteWorkSpaceModal';
 import InviteChannelModal from '@components/InviteChannelModal';
 import ChannelList from '@components/ChannelList';
 import DMList from '@components/DMList';
+import useSocket from '@hooks/useSocket';
 
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
@@ -56,11 +57,20 @@ const Workspace: VFC = () => {
     dedupingInterval: 2000,
   });
   const { data: channelData } = useSWR<IChannel[]>(userData ? `/api/workspaces/${workspace}/channels` : null, fetcher);
+  const { data: memberData } = useSWR<IUser[]>(userData ? `/api/workspaces/${workspace}/members` : null, fetcher);
+  const [socket, disconnect] = useSocket(workspace);
 
-  const { revalidate: memberData } = useSWR<IUser[]>(
-    userData ? `/api/workspaces/${workspace}/channels/members` : null,
-    fetcher,
-  );
+  useEffect(() => {
+    if (channelData && userData && socket) {
+      socket.emit('login', { id: userData.id, channels: channelData.map((v) => v.id) });
+    }
+  }, [socket, channelData, userData]);
+
+  useEffect(() => {
+    return () => {
+      disconnect();
+    };
+  }, [workspace, disconnect]);
 
   const onLogout = useCallback(() => {
     axios
