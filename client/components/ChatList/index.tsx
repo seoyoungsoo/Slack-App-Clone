12237b284c4567ff9f6e0fbed9b65d9c4f99c43f
seoyoungsoo <iulove37@naver.com>
2021-07-19 22:47:10 +0900

    Feat: 채팅 리스트 타입 설정 및 구현

--- client/components/ChatList/index.tsx
+++ client/components/ChatList/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { VFC } from 'react';
 import { ChatZone } from '@components/ChatList/styles';
+import { IDM } from '@typings/db';
+import Chat from '@components/Chat';
 
-const ChatList = () => {
-  return <ChatZone></ChatZone>;
+interface Props {
+  chatData?: IDM[];
+}
+const ChatList: VFC<Props> = ({ chatData }) => {
+  return (
+    <ChatZone>
+      {chatData?.map((chat) => (
+        <Chat key={chat.id} data={chat} />
+      ))}
+    </ChatZone>
+  );
 };
 
 export default ChatList;
