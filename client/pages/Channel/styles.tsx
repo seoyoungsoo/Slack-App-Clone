1abb13a0529533c019947ac5b390dca403f62f00
seoyoungsoo <iulove37@naver.com>
2021-07-25 21:06:09 +0900

    Style: 채널 css 추가

--- client/pages/Channel/styles.tsx
+++ client/pages/Channel/styles.tsx
@@ -17,4 +17,11 @@ export const Header = styled.header`
   padding: 20px 16px 20px 20px;
   font-weight: bold;
   align-items: center;
+
+  & .hearder-right {
+    display: flex;
+    flex: 1;
+    justify-content: flex-end;
+    align-items: center;
+  }
 `;
